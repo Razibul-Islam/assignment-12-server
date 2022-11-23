fa17b825e719bf47de55f4e068ff759749e3afad
Razibul Islam <razibulislam665@gmail.com>
2022-11-24 01:48:50 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5jjbyfi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+
+// async function run() {
+//   try {
+//   } finally {
+//   }
+// }
+// run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("doctors portal server is running");
+});
+
+
+app.listen(port, () => {
+    console.log('Port is running');
+})
